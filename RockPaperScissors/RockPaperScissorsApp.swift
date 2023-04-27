66384374dbb00bfa16182b76bb21c95aa8ce3a94
Anastasiia Solomka <solomken@gmail.com>
2023-04-27 16:13:21 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Anastasiia Solomka on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
